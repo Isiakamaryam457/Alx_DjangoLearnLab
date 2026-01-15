b09d60d94244b82cf975c67a5d6d78bd2251a5d6
Isiaka Maryam <isiakamaryam457@gmail.com>
2026-01-15 02:52:17 +0100

    modifying

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -11,7 +11,7 @@ books_by_author = Book.objects.filter(author=author)
 print("Books by", author.name, ":", [book.title for book in books_by_author])
 
 # 2. List all books in a library
-library = Library.objects.get(name="Central Library")
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 print("Books in", library.name, ":", [book.title for book in books_in_library])
 
