7d013861604be1112ac5156036c7fa78519f0a55
Isiaka Maryam <isiakamaryam457@gmail.com>
2026-01-18 23:59:38 +0100

    hgf

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register
+from .views import list_books, LibraryDetailView
+from . import views
 
 urlpatterns = [
     # Function-based view for listing books
@@ -12,6 +13,6 @@ urlpatterns = [
     # Authentication URL patterns
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 
 ]
