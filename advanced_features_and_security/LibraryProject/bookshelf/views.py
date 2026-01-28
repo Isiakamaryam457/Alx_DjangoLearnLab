6e02d441795dc73105808c9afab0603e98a0b7e4
Isiaka Maryam <isiakamaryam457@gmail.com>
2026-01-28 02:49:09 +0100

    ded

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -38,3 +38,9 @@ def delete_book(request, book_id):
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/list_books.html', {'books': books})
+
+@login_required
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
