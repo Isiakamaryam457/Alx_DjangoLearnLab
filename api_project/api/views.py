3e229be11197cc8061f22aa1de7b5545aaa7cfba
Isiaka Maryam <isiakamaryam457@gmail.com>
2026-01-30 01:56:30 +0100

    the last commit hopefully

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
@@ -7,7 +8,9 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer 
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset =Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
