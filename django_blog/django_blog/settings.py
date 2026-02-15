e40997b802d38a4462761ffe414aa0086db83220
Isiaka Maryam <isiakamaryam457@gmail.com>
2026-02-15 21:58:51 +0100

    modifying for postgre sql

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',        # Add this (empty for SQLite)
+        'PORT': '',        # Add this (empty for SQLite)
     }
 }
 
