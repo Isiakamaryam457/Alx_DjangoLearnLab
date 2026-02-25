c927d95372e612510d573ccc1fe0792f43b9864b
Isiaka Maryam <isiakamaryam457@gmail.com>
2026-02-25 07:27:27 +0100

    ertyh

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,32 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from .models import User
+
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'bio']
+        fields = ['username', 'email', 'password', 'bio', 'token']
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists.")
+        return value
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
             bio=validated_data.get('bio', '')
         )
-        Token.objects.create(user=user)
+        token, _ = Token.objects.get_or_create(user=user)
+        user.token = token.key
         return user
 
 
